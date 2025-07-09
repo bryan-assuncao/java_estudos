707720175bebf476b6d0eb226bc296b18bb4f03e
Bryan Assunção <bryan.assuncao@outlook.com.br>
2025-07-09 12:14:38 -0300

    feat: exercise 02 classes

---
+++ src/assuncao/bryan/codes/Test/BryanTest.java
@@ -0,0 +1,24 @@
+package assuncao.bryan.codes.Test;
+
+import assuncao.bryan.codes.classes.domain.Bryan;
+
+public class BryanTest {
+    public static void main(String[] args) {
+        Bryan bryan = new Bryan();
+
+        
+
+        bryan.printBryan("Djow", 24, "Java", "Python", "JavaScript", "C#");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/assuncao/bryan/codes/classes/domain/Bryan.java
@@ -0,0 +1,23 @@
+package assuncao.bryan.codes.classes.domain;
+
+public class Bryan {
+    public String surname;
+    public int age;
+    public String[] stacks;
+
+    public void printBryan(String surname, int age, String... stacks){
+
+        this.surname = surname;
+        this.age = age;
+        this.stacks = stacks;
+
+        System.out.println(this.surname);
+        System.out.println(this.age);
+        for(String stack : this.stacks){
+            System.out.println(stack);
+        }
+    }
+}
+
+
+
