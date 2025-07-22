ac72f4b338f8325c4f390fbeba426dd8b23c174a
Bryan Assunção <bryan.assuncao@outlook.com.br>
2025-07-22 12:27:29 -0300

    cost

---
+++ src/assuncao/bryan/codes/sobrescrita/domain/test/AnimeTest.java
@@ -0,0 +1,11 @@
+package assuncao.bryan.codes.sobrescrita.domain.test;
+
+import assuncao.bryan.codes.sobrescrita.domain.Anime;
+
+public class AnimeTest {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Demon Slayer");
+
+        System.out.println(anime.toString());
+    }
+}
---
+++ src/assuncao/bryan/codes/tipoprimitivo/domain/Car.java
@@ -0,0 +1,7 @@
+package assuncao.bryan.codes.tipoprimitivo.domain;
+
+public class Car {
+    private String name;
+    private final int LIMIT_SPEED = 110;
+
+}
