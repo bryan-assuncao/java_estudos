d518bcbd2ccb2ecd3d01c86564f762749d62040f
Bryan Assunção <bryan.assuncao@outlook.com.br>
2025-07-22 13:38:13 -0300

    modificador final

--- src/assuncao/bryan/codes/tipoprimitivo/domain/Buyer.java
+++ src/assuncao/bryan/codes/tipoprimitivo/domain/Buyer.java
@@ -1,9 +1,14 @@
 package assuncao.bryan.codes.tipoprimitivo.domain;
 
-public class Car {
+public class Buyer {
     private String name;
-    public static final int LIMIT_SPEED = 110;
-    public final Buyer BUYER = new Buyer();
+
+    @Override
+    public String toString() {
+        return "Buyer{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 
     public String getName() {
         return name;
@@ -12,6 +17,4 @@ public class Car {
     public void setName(String name) {
         this.name = name;
     }
-
-
 }
