3b1a461522926d530c474acbd8d31e93feba6829
Bryan Assunção <bryan.assuncao@outlook.com.br>
2025-08-06 22:19:08 -0300

    feat: começando weappers

--- src/assuncao/bryan/maratonajava/javacore/wrappers/test/WrappersTest.java
+++ src/assuncao/bryan/maratonajava/javacore/wrappers/test/WrappersTest.java
@@ -2,6 +2,26 @@ package assuncao.bryan.maratonajava.javacore.wrappers.test;
 
 public class WrappersTest {
     public static void main(String[] args) {
+        byte byteP = 1;
+        short shortP = 1;
+        int intP = 1;
+        long longP = 10L;
+        float floatP = 10F;
+        double doubleP = 10D;
+        char charP = 'P';
+        boolean boleanP  = false;
+        
+        Byte byteW = 1;
+        Short shortW = 1;
+        Integer intW = 1;
+        Long longW = 10L;
+        Float floatW = 10F;
+        Double doubleW = 10D;
+        Character charW = 'W';
+        Boolean boleanW  = false;
+
+
+        
 
     }
 }
