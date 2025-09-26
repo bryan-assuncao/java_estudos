7985e0c01de39b2ae9cac900328dc7f1690c0420
Bryan Conceição de Assunçao <bryan.assuncao@safeweb.com.br>
2025-09-26 16:45:23 -0300

    navigableset part 2

--- src/assuncao/bryan/maratonajava/javacore/W_Colecoes/test/NavigableSetTest.java
+++ src/assuncao/bryan/maratonajava/javacore/W_Colecoes/test/NavigableSetTest.java
@@ -14,6 +14,14 @@ class SmarthphoneMarcaComparator implements Comparator<Smarthphone>{
     }
 }
 
+class MangaPrecoComparador implements Comparator<Mangas>{
+
+    @Override
+    public int compare(Mangas o1, Mangas o2) {
+        return Double.compare(o1.getPreco(), o2.getPreco());
+    }
+}
+
 public class NavigableSetTest {
     public static void main(String[] args) {
         NavigableSet<Smarthphone> set = new TreeSet<>(new SmarthphoneMarcaComparator());
@@ -21,13 +29,32 @@ public class NavigableSetTest {
         Smarthphone iphone17 = new Smarthphone("A19", "Apple");
         set.add(iphone17);
 
-        NavigableSet<Mangas> mangas = new TreeSet<>();
+        NavigableSet<Mangas> mangas = new TreeSet<>(new MangaPrecoComparador());
 
         mangas.add(new Mangas(100L, "One Piece", 100, 0));
         mangas.add(new Mangas(29L, "Naruto", 29.9, 5));
         mangas.add(new Mangas(31L, "Attack on Titan", 19.9, 0));
         mangas.add(new Mangas(41L, "Eight Six", 15.9, 2));
+
+        Mangas yuyu = new Mangas(19L, "Akudama Drive", 30, 79);
+
+        System.out.println(mangas.lower(yuyu));
+
+        // lowe <
+        // flor <=
+        // higher
+        // ceiling >=
+
+        System.out.println("------------------------------------------------------------------");
+
+
         for (Mangas manga : mangas) {
+            System.out.println(manga);
+        }
+
+        System.out.println("------------------------------------------------------------------");
+
+        for (Mangas manga : mangas.descendingSet()) {   // fazer ao contrário
             System.out.println(manga);
         }
 
