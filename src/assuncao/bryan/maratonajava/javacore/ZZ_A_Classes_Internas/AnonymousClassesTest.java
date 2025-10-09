9e016880a4b5e0568af6b2fb1097188c39be4cfa
Bryan Conceição de Assunçao <bryan.assuncao@safeweb.com.br>
2025-10-09 16:58:25 -0300

    anonymous classes

---
+++ src/assuncao/bryan/maratonajava/javacore/ZZ_A_Classes_Internas/AnonymousClassesTest.java
@@ -0,0 +1,25 @@
+package assuncao.bryan.maratonajava.javacore.ZZ_A_Classes_Internas;
+
+class Animal{
+    public void walk(){
+        System.out.println("Animal walking...");
+    }
+}
+
+
+public class AnonymousClassesTest {
+    public static void main() {
+
+        Animal animal = new Animal(){
+            @Override
+            public void walk() {
+                System.out.println("Walking in the shadows...");
+            }
+        };
+
+
+        animal.walk();
+    }
+}
+
+
--- src/assuncao/bryan/maratonajava/javacore/ZZ_A_Classes_Internas/OuterClassesTest.java
+++
@@ -1,9 +0,0 @@
-package assuncao.bryan.maratonajava.javacore.ZZ_A_Classes_Internas;
-
-public class OuterClassesTest {
-    private String name = "Naruto Uzumaki";
-
-    public static void main(String[] args) {
-
-    }
-}
